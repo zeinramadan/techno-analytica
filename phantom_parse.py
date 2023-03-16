d2f369c4eb505c3ba1c46ee2c77994923fcea267
Zein Ramadan <zein.ramadan2@gmail.com>
2023-03-16 20:27:24 +0000

    add looping

--- phantom_parse.py
+++ phantom_parse.py
@@ -35,13 +35,30 @@ def get_profile_description(profile_url):
 
 if __name__ == '__main__':
 
-    # Read the CSV file
-    file_path = "result_short.csv"
-    df = pd.read_csv(file_path)
+    # Given a collection of CSV files containing the profileUrls of each follower for 1 instagram account, loop through
+    # and add the bio to each CSV file
+    input_folder_path = "phantom_csv_files"
+    output_folder_path = "augmented_csvs"
+    csv_files = [f"{input_folder_path}/result.csv"]
 
-    # Apply the get_profile_description() function to each row in the DataFrame
-    # This will loop through all profile URLs and fetch the bio for each URL
-    df['Bio'] = df['profileUrl'].apply(get_profile_description)
+    print("Accounts to iterate through: {}".format(len(csv_files)))
 
-    # Save the modified DataFrame to a new CSV file
-    df.to_csv("output_with_bio.csv", index=False)
+    counter = 1
+    for csv_file in csv_files:
+
+        # Log which account we are processing
+        print("Starting collection for {}. Number {} out of {}".format(csv_file, counter, len(csv_files)))
+        counter += 1
+
+        df = pd.read_csv(csv_file)
+
+        # Apply the get_profile_description() function to each row in the DataFrame
+        # This will loop through all profile URLs and fetch the bio for each URL
+        df['Bio'] = df['profileUrl'].apply(get_profile_description)
+
+        # Save the modified DataFrame to a new CSV file
+        df.to_csv(f"{output_folder_path}/{csv_file}_with_bios.csv", index=False)
+
+        # Log completion
+        print("Run complete for {}".format(csv_file))
+        print("===============================")
